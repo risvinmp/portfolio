dec7be21400448dffe2693e397246e9eceda2891
risvinmp <risvinmp93@gmail.com>
2022-09-27 20:37:11 +0530

    hero section changed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import {Cursor, useTypewriter } from 'react-simple-typewriter';
-import BackgroundCircles from './BackgroundCircles'
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
+import BackgroundCircles from './BackgroundCircles';
+import Image from 'next/image';
+
 
 type Props = {}
 
@@ -21,12 +23,20 @@ export default function Hero({ }: Props) {
 
 
     return (
-       <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
-        <BackgroundCircles />
-        <h1>
-            <span>{ text }</span>
-            <Cursor cursorColor='#F7AB0A'></Cursor>
-        </h1>
-       </div>
+        <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+            <BackgroundCircles />
+            <img
+                className='relative rounded-full h-36 w-36 mx-auto object-cover'
+                src="/r-1.jpg" alt="" />
+
+            <div>
+                <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Developer</h2>
+                <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+                    <span className='mr-3'>{text}</span>
+                    <Cursor cursorColor='#F7AB0A'></Cursor>
+                </h1>
+            </div>
+
+        </div>
     )
 } 
