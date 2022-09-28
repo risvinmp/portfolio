9a3b69b3a24b9a870654ef2c84604882c63ca831
risvinmp <risvinmp93@gmail.com>
2022-09-28 09:14:28 +0530

    about section started

---
+++ components/About.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { motion } from "framer-motion";
+
+type Props = {}
+
+export default function About({}: Props) {
+  return (
+    <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
+
+       <motion.img 
+
+       initial={{
+        x: -200,
+        operator: 0,
+       }}
+
+       transition={{
+        duration: 1.2,
+       }}
+       whileInView={{ opacity: 1, x: 0 }}
+       viewport={{ once: true }}
+
+       src='/risvin_1.png'
+       className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover 
+       md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[300px]'
+       />
+        </div>
+  )
+}
