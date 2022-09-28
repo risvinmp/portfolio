a739fcb8189abe430a6431b37fcd9412b0109517
risvinmp <risvinmp93@gmail.com>
2022-09-28 08:07:44 +0530

    hero section completed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
 import Image from 'next/image';
+import Link from 'next/link';
 
 
 type Props = {}
@@ -29,14 +30,31 @@ export default function Hero({ }: Props) {
                 className='relative rounded-full h-36 w-36 mx-auto object-cover'
                 src="/r-1.jpg" alt="" />
 
-            <div>
+            <div className='z-20'>
                 <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Developer</h2>
                 <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
                     <span className='mr-3'>{text}</span>
                     <Cursor cursorColor='#F7AB0A'></Cursor>
                 </h1>
-            </div>
 
+                <div className='pt-5'>
+                    <Link href="#about">
+                    <button className='heroButton'>About</button>
+                    </Link>
+                    
+                    <Link href="#experience">
+                    <button className='heroButton'>Experience</button>
+                    </Link>
+
+                    <Link href="#skills">
+                    <button className='heroButton'>Skills</button>
+                    </Link>
+                    
+                    <Link href="#projects">
+                    <button className='heroButton'>Projects</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 } 
